495cc60e658bc4ead18b06da66169cbc25584bb7
Nondukishor <nipuchakraorty86@hotmail.com>
2022-11-22 00:01:59 +0600

    init

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     // Run format:fix on changes to all files
-    "**/*.(ts|js|json)?(x)": () => "yarn format",
+    "/src/**/*.(ts|js|json)?(x)": () => "yarn format",
     // Run type-check on changes to TypeScript files
-    "**/*.(ts)?(x)": () => "yarn type-check",
+    "/src/**/*.(ts)?(x)": () => "yarn type-check",
     // Run ESLint on changes to JavaScript/TypeScript files
-    "**/*.(ts|js)?(x)": (filenames) =>
+    "/src/**/*.(ts|js)?(x)": (filenames) =>
       `yarn lint . ${filenames.join(" ")}`,
   };
