e1fc122ef0768c83b41092b48150c0e17b7fbca0
Nondukishor <nipuchakraorty86@hotmail.com>
2022-12-03 01:57:41 +0600

    @nestjs/config,@nestjs/jwt, @nestjs/passport, @types/passport-jwt, moment, moment-timezone, mysql2, passport, passport-jwt, passport-local installed

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,5 +1,8 @@
 
-import { Resolver, Query  } from '@nestjs/graphql';
+import { UnauthorizedException } from '@nestjs/common';
+import { Resolver, Query, Mutation, Args  } from '@nestjs/graphql';
+import { Itoken } from './dto/types';
+import { CreateUserInput, UserCredentialInput } from './dto/User';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 
@@ -8,6 +11,23 @@ export class UserResolver {
     constructor(private userService : UserService){}
     @Query(returns=>[User])
     users():Promise<User[]>{
+        const user = new User()
         return this.userService.findAll()
+    }
+
+
+    @Mutation(returns=>User)
+    createUser(@Args('createUserInput') createUserInput: CreateUserInput){
+      return this.userService.signUp(createUserInput)
+    }
+
+    @Mutation(returns=>Itoken)
+    signIn(@Args('userCredentialInput') userCredentialInput: UserCredentialInput):Promise<Itoken>{
+      try {
+        return this.userService.signIn(userCredentialInput)
+      } catch (error) {
+        console.log(error)
+      }
+      
     }
 }
